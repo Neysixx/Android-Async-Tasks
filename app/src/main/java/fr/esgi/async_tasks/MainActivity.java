eeefb135dcbef9e16e73a5c52e21f310707e39d2
Neysixx <kylliansenrens3004@gmail.com>
2024-06-06 12:50:07 +0200

    End TP

--- app/src/main/java/fr/esgi/async_tasks/MainActivity.java
+++ app/src/main/java/fr/esgi/async_tasks/MainActivity.java
@@ -1,6 +1,8 @@
 package fr.esgi.async_tasks;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.ref.WeakReference;
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String TEXT_STATE = "currentText";
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView1);
+        if (savedInstanceState != null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void startTask(View view) {
+        mTextView = (TextView) findViewById(R.id.textView1);
+        new SimpleAsyncTask(mTextView).execute();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
     }
 }
